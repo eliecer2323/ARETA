2de254d924ea0a8fdcc22bc0963fc27bbec90901
JorgeEliecerSierra <JorgeEliecerSierra@DESKTOP-TPCCM3T>
2017-03-29 22:13:10 -0500

    primer commit

---
+++ evento/src/org/evento/Nuevo.java
@@ -0,0 +1,12 @@
+package org.evento;
+
+import gov.logica.IProgramacion;
+
+public class Nuevo implements IProgramacion {
+
+	@Override
+	public void programarEvento() {
+		System.out.println("se programa eventos");
+	}
+ 
+}
---
+++ evento/src/org/evento/Prueba.java
@@ -0,0 +1,9 @@
+package org.evento;
+
+public class Prueba {
+	
+	public void probar(){
+		System.out.println("estoy probando");
+	}
+	
+}
